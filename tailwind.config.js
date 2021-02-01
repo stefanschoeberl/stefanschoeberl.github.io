0de152ae0b7b169fecab6b5cc8bd3ddf4dc3a0e2
Stefan Schöberl <10100114+stefanschoeberl@users.noreply.github.com>
2021-02-01 22:04:09 +0100

    Add blue line in current menu item

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
+const plugin = require('tailwindcss/plugin')
+const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
+const nameClass = require('tailwindcss/lib/util/nameClass').default;
+const lodash = require('lodash');
+
 module.exports = {
   purge: [],
   darkMode: false,
@@ -29,7 +34,22 @@ module.exports = {
       padding: ['first', 'last'],
     },
   },
-  plugins: [],
+  plugins: [
+      plugin(function({ addUtilities, theme, variants }) {
+        const colors = lodash.omit(flattenColorPalette(theme('borderColor')), 'DEFAULT');
+
+        const getProperties = (baseName, cssProperty) => {
+          return lodash.fromPairs(lodash.map(colors, (value, modifier) => {
+            return [nameClass(baseName, modifier), {[cssProperty]: value}]
+          }));
+        }
+
+        addUtilities(getProperties('border-t', 'border-top-color'), variants('borderColor'));
+        addUtilities(getProperties('border-r', 'border-right-color'), variants('borderColor'));
+        addUtilities(getProperties('border-b', 'border-bottom-color'), variants('borderColor'));
+        addUtilities(getProperties('border-l', 'border-left-color'), variants('borderColor'));
+      }),
+  ],
   corePlugins: {
     container: false
   }
