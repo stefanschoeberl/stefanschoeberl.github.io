030952080cb579d5bc58c8c823eb3c2f486b316c
Stefan Schöberl <10100114+stefanschoeberl@users.noreply.github.com>
2021-02-18 20:43:53 +0100

    Optimize CSS, cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-const plugin = require('tailwindcss/plugin')
-const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
-const nameClass = require('tailwindcss/lib/util/nameClass').default;
-const lodash = require('lodash');
-
 module.exports = {
-  purge: [],
+  purge: [
+      './**/*.html',
+      './js/**/*.js',
+  ],
   darkMode: false,
   theme: {
     extend: {
@@ -29,26 +27,9 @@ module.exports = {
       }
     },
   },
-  variants: {
-    extend: {
-      padding: ['first', 'last'],
-    },
-  },
+  variants: {},
   plugins: [
-      plugin(function({ addUtilities, theme, variants }) {
-        const colors = lodash.omit(flattenColorPalette(theme('borderColor')), 'DEFAULT');
-
-        const getProperties = (baseName, cssProperty) => {
-          return lodash.fromPairs(lodash.map(colors, (value, modifier) => {
-            return [nameClass(baseName, modifier), {[cssProperty]: value}]
-          }));
-        }
-
-        addUtilities(getProperties('border-t', 'border-top-color'), variants('borderColor'));
-        addUtilities(getProperties('border-r', 'border-right-color'), variants('borderColor'));
-        addUtilities(getProperties('border-b', 'border-bottom-color'), variants('borderColor'));
-        addUtilities(getProperties('border-l', 'border-left-color'), variants('borderColor'));
-      }),
+      require('./plugins/border'),
   ],
   corePlugins: {
     container: false
